8b932e80a11c1926d7033062c01f27e3bef2609e
EliJordanZeno <eli.jordan@zenogroup.com>
2023-08-02 13:47:28 -0400

    pseudocode for create_links

--- create_links.py
+++ create_links.py
@@ -1,5 +1,13 @@
 import openpyxl as pxl
-
+'''
+1. find questions and options cells in homepage sheet
+    a. search through cells in column A in homepage
+    b. if cell below a Q## is empty, move over and down one to find Option1, then add to DICT with Q##O1 as index
+    c. if cell below a Q## is not empty, add Q## to a DICT
+2. find questions in options cells in crosstab sheet
+3. link homepage CELL to crosstab CELL
+4. format homepage cell to represent link (blue and underlined)
+'''
 file_name = "./test_file.xlsx"
 wb = pxl.load_workbook(file_name)
 
